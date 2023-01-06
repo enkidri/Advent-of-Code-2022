06aea12e8784fe3038000b90869719bef1b060f5
Tai Ta <lawliet@hotmail.se>
2023-01-06 14:19:35 +0100

    Day 16 solution

--- day16/day16.cc
+++ day16/day16.cc
@@ -25,6 +25,13 @@ using namespace std;
 #define INF 999
 typedef pair<int, vector<string>> Value;
 
+// Part 1: Dynamic programming over nonreduced graph
+// Part 2: Dynamic programming over reduced graph + combinatorics over possible
+//         significant valve configuration. I do not like the implementation since
+//         I'm making hard assumptions about the number of valves I and the elephant
+//         are visiting (me visits # significant valves/2, elephant visits the rest)
+//         but oh well it works.
+
 map<string,Value> parse(ifstream& file)
 {
     string input{};
@@ -57,9 +64,7 @@ map<string,Value> parse(ifstream& file)
     return m;
 }
 
-
-
-int find_max(map<vector<string>,int>& cache, map<string,Value>& map, string start, 
+int find_max(map<vector<string>, int>& cache, map<string,Value>& map, string start, 
              set<string> open_valves, int cost_to_go, int rate_i, int min_left)
 {
     vector<string> state = {to_string(cost_to_go), to_string(min_left), start};
@@ -110,7 +115,7 @@ vector<vector<int>> floydWarshall(vector<vector<int>>& m)
         {
             for (int j=0; j < v.size(); j++)
             {
-                if (v[i][k] + v[k][j] < m[i][j])
+                if (v[i][k] + v[k][j] < v[i][j])
                     v[i][j] = v[i][k] + v[k][j];
             }
         }
@@ -118,6 +123,8 @@ vector<vector<int>> floydWarshall(vector<vector<int>>& m)
     
     return v;
 }
+
+void printMatrix(vector<vector<int>>& m);
 
 vector<vector<int>> calcDistances(map<string,Value>& m)
 {
@@ -139,6 +146,7 @@ vector<vector<int>> calcDistances(map<string,Value>& m)
         }
         idx++;
     }
+    
     vector<vector<int>> m_floyd = floydWarshall(distance_matrix);
     return m_floyd;
 }
@@ -186,7 +194,7 @@ void partOne()
 {   
     ifstream file("day16.txt");
     map<string,Value> parsed = parse(file);
-    map<vector<string>,int> cache;
+    map<vector<string>, int> cache;
 
     string start = "AA";
     int goal_iter = 30;
@@ -200,36 +208,123 @@ void partOne()
 
 }
 
-//TODO: Carry out DP with this reduced graph. Pretty much the same as
-// findMax function.
-int maxSearchReduced(vector<vector<int>>& distances, vector<int> working_valves, 
-                     vector<string> sequence, int time_left, int cost_to_go)
+int maxSearchReduced(map<tuple<int,int,int>,pair<int,set<int>>>& cache, map<string,Value>& parsed,
+                     vector<vector<int>>& distances, vector<int> working_valves, 
+                     vector<int> sequence, int current, int time_left, int cost_to_go)
 {
-    if (time_left <= 0)
+    tuple<int,int,int> state = {cost_to_go, time_left,
+                                current};
+    if ((time_left <= 0) || (working_valves.empty())) 
         return cost_to_go;
+    else if(cache.find(state) != cache.end())
+        return cache[state].first;
 
     int max_pressure{};
-    for (auto& valve_idx:working_valves)
+    for (int i=0; i < working_valves.size(); i++)
     {
+        vector<int> seq_temp = sequence;
+        seq_temp.push_back(working_valves[i]);
+        vector<int> valves_left_temp = working_valves;
+        valves_left_temp.erase(valves_left_temp.begin() + i);
+        int time_left_temp = time_left;
+        int cost_to_go_temp = cost_to_go;
+        auto it_rate = parsed.begin(); 
+        advance(it_rate, working_valves[i]);
+        int rate = (*it_rate).second.first;
 
+        time_left_temp -= distances[current][working_valves[i]];   //time moving to significant valve
+        time_left_temp--;  //time opening the valve
+        cost_to_go_temp += rate * time_left_temp;
+
+        int max_temp{};
+        if (time_left_temp < 0)
+        {
+            max_temp = cost_to_go;
+        }
+        else
+        {
+            max_temp = maxSearchReduced(cache, parsed, distances, valves_left_temp, 
+                                        seq_temp, working_valves[i], time_left_temp, 
+                                        cost_to_go_temp);
+        }
+
+        if (max_temp > max_pressure)
+            max_pressure = max_temp;
     }
+
+    set<int> s(sequence.begin(), sequence.end());
+    tuple<int, int, int> key = {cost_to_go, time_left, current};
+    cache[key] = make_pair(max_pressure, s);
 
     return max_pressure;
 }
 
+vector<int> symmetric_difference(vector<int> s1, vector<int> s2)
+{
+    sort(s1.begin(), s1.end());
+    sort(s2.begin(), s2.end());
+
+    vector<int> res;
+    set_symmetric_difference(s1.begin(), s1.end(),
+                             s2.begin(), s2.end(),
+                             back_inserter(res));
+
+    return res;
+}
+
+//Gets combination of significant valves without repititions.
+void combinatorics(vector<int>& comb, vector<int>& valves,
+                   int offset, int k, vector<vector<int>>& output)
+{
+    if (k == 0)
+    {
+        output.push_back(comb);
+        return;
+    }
+
+    for (int i = offset; i <= valves.size() - k; i++)
+    {
+        comb.push_back(valves[i]);
+        combinatorics(comb, valves, i+1, k-1, output);
+        comb.pop_back();
+    }
+}
+
 void partTwo()
 {
-    //Same as part one (aka with DP) but with reduced graph over significant valves
+    //Same as part one (aka with DP) but with reduced graph over significant valves.
+    //Split all significant valve comibations into two sets, remove duplicates 
+    //and run the algorithm over it.
     ifstream file("day16.txt");
     map<string,Value> parsed = parse(file);
     vector<vector<int>> v = calcDistances(parsed);
     vector<int> working_valves = getWorkingValves(parsed);
+    map<tuple<int,int,int>,pair<int,set<int>>> cache;                        //key: valves visited + current valve item: cost-to-go
+    vector<int> sequence;
+    int current = 0;                                          //First valve has index 0
+    int time_left{26};
 
-    vector<string> sequence;
-    int time_left{30};
-    int max_pressure = maxSearchReduced(v, working_valves, sequence, time_left, 0);
-    
-    cout << "Part 2: The maximum pressure with two agents is " << max_pressure << endl;
+    vector<int> comb;
+    vector<int> valves(working_valves);
+    int k = working_valves.size() / 2;
+    vector<vector<int>> output;
+    combinatorics(comb, valves, 0, k, output);
+
+    int max{};
+    for (vector<int>& vec:output)
+    {
+        vector<int> s1 = vec;
+        vector<int> s2 = symmetric_difference(working_valves, vec);
+        cache = {};
+        int m1 = maxSearchReduced(cache, parsed, v, s1, sequence, current, time_left, 0);
+        cache = {};
+        int m2 = maxSearchReduced(cache, parsed, v, s2, sequence, current, time_left, 0);
+
+        if (m1 + m2 > max)
+            max = m1 + m2;
+    }
+
+    cout << "Part 2: The maximum pressure with two agents is " << max << endl;
 }
 
 int main()
